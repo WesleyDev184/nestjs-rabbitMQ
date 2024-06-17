4429b6833afd5f8507c8d5473a6f68d674e2c858
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2024-06-16 21:40:02 -0300

    init

---
+++ consumer/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://admin:admin@localhost:5672'],
+        queue: 'orders-queue',
+      },
+    },
+  );
+  app.listen();
+}
+bootstrap();
---
+++ producer/src/orders/order.dto.ts
@@ -0,0 +1,5 @@
+export class OrderDto {
+  email: string;
+  productName: string;
+  quantity: number;
+}
